33a7d121b9a0be5ce440ca9938f13f9a9d0d8640
Coru89 <corey.james.keller@gmail.comcom>
2022-10-26 22:45:23 +0700

    more gulp config, primary + mobile nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 const { src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass')); // This is different from the video since gulp-sass no longer includes a default compiler. Install sass as a dev dependency `npm i -D sass` and change this line from the video.
 const prefix = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
-// const terser = require('gulp-terser');
+const uglify = require('gulp-uglify');
+const typescript = require('gulp-typescript');
 // const imagemin = require('gulp-imagemin');
 // const imagewebp = require('gulp-webp');
 
@@ -11,8 +13,20 @@ const minify = require('gulp-clean-css');
 
 // watch
 function watchTask(){
+	browserSync.init({
+		proxy: {
+			target: "https://tangent.ddev.site/",
+			proxyRes: [
+				function(proxyRes, req, res) {
+					console.log(proxyRes.headers);
+				}
+			]
+    	}
+	});
+
+	watch('templates/**/*.twig').on('change', browserSync.reload);
 	watch('source/styles/**/*.scss', scss); // change to your source directory
-	// watch('src/js/*.js', jsmin); // change to your source directory
+	watch('source/scripts/*.ts', ts); // change to your source directory
 	// watch('src/images/*', optimizeimg); // change to your source directory
 	// watch('dist/images/*.{jpg,png}', webpImage); // change to your source directory
 }
@@ -24,6 +38,7 @@ function scss() {
 	  .pipe(prefix('last 2 versions'))
 	  .pipe(minify())
 	  .pipe(dest('./web/assets/css')) // change to your final/public directory
+	  .pipe(browserSync.stream());
   };
 
 //optimize and move images
@@ -44,20 +59,22 @@ function scss() {
 // };
 
 
-// minify js
-// function jsmin(){
-//   return src('src/js/*.js') // change to your source directory
-//     .pipe(terser())
-//     .pipe(dest('dist/js')); // change to your final/public directory
-// }
+//compile ts and minify
+function ts() {
+    return src('./source/scripts/*.ts')
+    .pipe(typescript())
+    .pipe(uglify())
+    .pipe(dest('./web/assets/scripts'))
+	.pipe(browserSync.stream());
+}
 
-//gulp.task('default', gulp.series('scss', 'watchTask'));
-
+exports.watchTask = watchTask;
+exports.ts = ts;
 exports.scss = scss;
 // Default Gulp task 
 exports.default = series(
 	scss,
-	// jsmin,
+	ts,
 	// optimizeimg,
 	// webpImage,
 	watchTask
